f5754cc283657b4bc6040039d9f71b414768760c
Dinu Barbu <dinu@DevPlants-MacBook-Pro.local>
2020-03-03 13:28:22 +0200

    insert landing page into own component. menu button interactive

---
+++ src/app/landing-page/landing-page.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-page',
+  templateUrl: './landing-page.component.html',
+  styleUrls: ['./landing-page.component.scss']
+})
+export class LandingPageComponent implements OnInit {
+  showMenu: boolean = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  toggleShowMenu() {
+    this.showMenu = !this.showMenu;
+  }
+}
