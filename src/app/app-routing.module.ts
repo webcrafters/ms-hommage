7cb830d201d29b0949f116a3a6ff5b0e0c5794d9
Dinu Barbu <dinu@DevPlants-MacBook-Pro.local>
2020-03-03 19:13:12 +0200

    add routing, integrate with app case

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LandingShowcaseComponent } from './landing-page/landing-showcase/landing-showcase.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'ms-home', component: LandingPageComponent },
+  { path: '', component: LandingPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
